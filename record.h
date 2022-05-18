c911f35ef40489d442c6fbde90986907747e1407
Royer Jack <jack.royer.23@gmail.com>
2022-05-18 09:33:40 +0200

    records progress

--- record.h
+++ record.h
@@ -1,3 +1,6 @@
+#pragma once
+#include <stdlib.h>
+
 /**
     Handles the general record type that abstracts away from the actual type of the data.
 
@@ -10,12 +13,12 @@
 
 struct record
 {
-    int type;
+    size_t type;
     void *data;
 };
 
-
-enum record_type{
+enum record_type
+{
     RECORD_TYPE_NULL,
     RECORD_TYPE_CHAR,
     RECORD_TYPE_SHORT,
@@ -26,22 +29,20 @@ enum record_type{
     RECORD_TYPE_1,
 };
 
-/**
- * @brief Gets the size of the record. The size is given by the table in the documentation.
- *
- * @param record
- * @return int
- */
-int record_size(struct record *record);
+#define LAST_RECORD_TYPE RECORD_TYPE_1
 
-char record_get_char(struct record *record);
+size_t record_size(struct record *record);
 
-short record_get_short(struct record *record);
+char *record_get_char(struct record *record);
+short *record_get_short(struct record *record);
+int *record_get_int(struct record *record);
+long *record_get_long(struct record *record);
+float *record_get_float(struct record *record);
+char **record_get_string(struct record *record);
 
-int record_get_int(struct record *record);
-
-long record_get_long(struct record *record);
-
-float record_get_float(struct record *record);
-
-char *record_get_string(struct record *record);
+struct record *record_from_char(char *data);
+struct record *record_from_short(short *data);
+struct record *record_from_int(int *data);
+struct record *record_from_long(long *data);
+struct record *record_from_float(float *data);
+struct record *record_from_string(char **data);
