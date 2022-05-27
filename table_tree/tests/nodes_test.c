4b97e748ac18d947f9ef2d621ceb80372f70d54b
Sharujan <sharus8@yahoo.fr>
2022-05-27 11:08:28 +0200

    yabgetall not working yet

--- table_tree/tests/nodes_test.c
+++ table_tree/tests/nodes_test.c
@@ -2,43 +2,74 @@
 
 
 int main(){
-  struct data tdata2 = {"John","Doe",69};
-  struct data tdata3 = {"Jahn","Doe",34};
-  struct data tdata4 = {"Jihn","Doe",33};
-  struct data tdata5 = {"Juhn","Doe",55};
-  struct data tdata6 = {"Jehn","Doe",82};
-  struct data tdata7 = {"Jane","Doe",99};
-  struct data tdata8 = {"Joe","Doe",12};
+  struct data tdata1 = {"John","Doe",69};
+  struct data tdata2 = {"Jahn","Doe",34};
+  struct data tdata3 = {"Jihn","Doe",33};
+  struct data tdata4 = {"Juhn","Doe",55};
+  struct data tdata5 = {"Jehn","Doe",82};
+  struct data tdata6 = {"Jane","Doe",99};
+  struct data tdata7 = {"Joe","Doe",12};
+  struct data tdata8 = {"Jhin", "Doe", 21};
+  struct data tdata9 = {"Jim", "Doe", 61};
+  struct data tdata10 = {"Jun", "Doe", 41};
+  struct data tdata11 = {"Jan", "Doe", 51};
+  struct data tdata12 = {"Jan", "Doe", 21};
 
   btree ttest = yabcreate();
-  struct key_value k1 = {1,&tdata2};
+  struct key_value k1 = {1,&tdata1};
   struct key_value k2 = {2,&tdata2};
-  struct key_value k3 = {3,&tdata2};
-  struct key_value k4 = {4,&tdata2};
-  struct key_value k5 = {5,&tdata2};
-  struct key_value k6 = {6,&tdata2};
-  struct key_value k7 = {7,&tdata2};
-  struct key_value k8 = {8,&tdata2};
+  struct key_value k3 = {3,&tdata3};
+  struct key_value k4 = {4,&tdata4};
+  struct key_value k5 = {5,&tdata5};
+  struct key_value k6 = {6,&tdata6};
+  struct key_value k7 = {7,&tdata7};
+  struct key_value k8 = {8,&tdata8};
+  struct key_value k9 = {9,&tdata9};
+  struct key_value k10 = {10,&tdata10};
+  struct key_value k11 = {11,&tdata11};
+  struct key_value k12 = {12,&tdata11};
 
 
 
-
-  ttest = yabinsert(ttest, k1);
-  ttest = yabinsert(ttest, k2);
-  ttest = yabinsert(ttest, k3);
-  ttest = yabinsert(ttest, k4);
-  ttest = yabinsert(ttest, k5);
-  ttest = yabinsert(ttest, k6);
-  ttest = yabinsert(ttest, k7);
-  ttest = yabinsert(ttest, k8);
-  ttest = yabinsert(ttest, k8);
+  yabinsert(ttest, k1);
+  yabinsert(ttest, k2);
+  yabinsert(ttest, k3);
+  yabinsert(ttest, k4);
+  yabinsert(ttest, k5);
+  yabinsert(ttest, k6);
+  yabinsert(ttest, k7);
+  yabinsert(ttest, k8);
+  yabinsert(ttest, k9);
+  yabinsert(ttest, k10);
+  yabinsert(ttest, k11);
+  yabinsert(ttest, k12);
   struct data *result;
   result = malloc(sizeof(*result));
-  result = yabsearch(ttest,1);
+  result = yabsearch(ttest,6);
   printf("Nom : %s, Prenom : %s, Age :%d\n",result->nom,result->prenom,result->age);
-
-  ttest = yabdelete(ttest,4);
-
+  
+  printf("CCCCCCCCCCCC  %d \n", ttest->children[0]->is_leaf);
+  
+  yabdelete(ttest,2);
+  
 
   node_to_string(ttest);
+  for (int i=0; i<ttest->nb_keys+1;i++){
+    printf("LEVEL 2---------\n");
+    node_to_string(ttest->children[i]);
+    printf("LEVEL 3------------\n");
+    for (int i2=0;i2<<ttest->children[i]->nb_keys;i2++){
+      node_to_string(ttest->children[i]->children[i2]);
+    }
+    printf("-------------------\n");
+  }
+  dataarray *all;
+  all = malloc(sizeof(*all));
+  all->size =0;
+  all->data = malloc(sizeof(*(all->data)));
+  printf("%d\n", all->size);
+
+
+  yabgetall(ttest,all);
+  printf("%d\n", all->size);
 }
